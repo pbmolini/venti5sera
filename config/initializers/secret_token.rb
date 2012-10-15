27f7504757f3c8e9864411d1dca5982d74d10358
pbmolini <pbmolini@fbk.eu>
2012-10-15 17:23:50 +0200

    Tutto il resto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Venti5sera::Application.config.secret_token = '73e439bc958acf82ee174d5bd54a18192db80a041147679d959d366e2f44fa8a564983d577b366716a2d3db877d7826050034c95d020cb3a3cc92f7b8a74b999'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Venti5sera::Application.config.session_store :cookie_store, key: '_venti5sera_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Venti5sera::Application.config.session_store :active_record_store
